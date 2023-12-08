bb114149eeeeddbaed3ffbde499c5cd086dc45e3
Chyngyz <konuratov.chika@gmail.com>
2023-12-08 17:36:51 +0600

    Done

--- todo/webapp/views.py
+++ todo/webapp/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+
+from webapp.models import Task
+from webapp.serializer import TaskSerializer
+
 
 # Create your views here.
+
+class TaskViewSet(viewsets.ModelViewSet):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
